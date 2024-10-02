7e70eba789e5cbe0b1affee6e2712d0f9e8ce01e
Marcello Yoel Christianus <marcelloyoell@gmail.com>
2024-10-02 10:57:53 +0700

    finish

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import ProductCard from '@/components/ProductCard.vue'
+import ProductCard from '@/components/ProductCard.vue';
 import Pagination from '@/components/Pagination.vue';
 import Loading from '@/components/Loading.vue';
-import { onMounted, ref, watch, watchEffect } from 'vue';
+import ProductForm from '@/components/ProductForm.vue';
 
+import { ref, watchEffect } from 'vue';
 import axios from 'axios';
 
 const products = ref([]);
@@ -11,40 +12,58 @@ const page = ref(1);
 const limit = ref(8);
 const isLoading = ref(true);
 
-async function fetchData(){
+async function fetchData() {
 	const API_URL = `http://localhost:3000/products?_page=${page.value}&_per_page=${limit.value}`;
 	try {
 		isLoading.value = true;
-		products.value = await axios.get(API_URL).then((res) => res.data);
+		const response = await axios.get(API_URL);
+		products.value = response.data;
 	} catch (error) {
-		console.log(error);
-	} finally{
+		console.error(error);
+	} finally {
 		isLoading.value = false;
 	}
 }
 
 watchEffect(() => {
 	fetchData();
-})
+});
 
-function changePage(newPage){
-	if(newPage < 1) return;
-	if(newPage > products.value.pages) return;
+function changePage(newPage) {
+	if (newPage < 1) return;
+	if (newPage > products.value.pages) return;
 	page.value = newPage;
 }
 
+async function createProduct(product) {
+	try {
+		await axios.post('http://localhost:3000/products', product);
+		fetchData();
+	} catch (error) {
+		console.error(error);
+	}
+}
 </script>
 
 <template>
 	<div v-if="isLoading">
-		<Loading></Loading>
+		<Loading />
 	</div>
 	<main v-else>
+		<ProductForm @create-product="createProduct" />
 		<div class="product-grid">
-    		<ProductCard v-for="(product, index) in products.data" :key="index" :product="product"></ProductCard>
+			<ProductCard
+				v-for="(product, index) in products.data"
+				:key="index"
+				:product="product"
+			/>
 		</div>
 		<div class="pagination">
-			<Pagination :page="page" :totalPage="products.pages" @change-page="changePage"></Pagination>
+			<Pagination
+				:page="page"
+				:totalPages="products.pages"
+				@change-page="changePage"
+			/>
 		</div>
 	</main>
 </template>
@@ -57,6 +76,7 @@ function changePage(newPage){
 	width: 80%;
 	margin: 0 auto;
 }
+
 .pagination {
 	display: flex;
 	justify-content: center;
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,40 +1,61 @@
-<script>
-import {onMounted, ref} from 'vue';
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
-import {useRoute} from 'vue-router';
+import ProductForm from '@/components/ProductForm.vue';
 
 const route = useRoute();
+const router = useRouter();
 
 const id = route.params.id;
 const product = ref({});
+const API_URL = `http://localhost:3000/products/${id}`;
 
 onMounted(() => {
 	fetchData();
 });
 
-async function fetchData(){
-	const API_URL = `http://localhost:3000/products/${id}`;
+async function fetchData() {
 	try {
 		const response = await axios.get(API_URL);
 		product.value = response.data;
 	} catch (error) {
-		console.log(error);
+		console.error(error);
+	}
+}
+
+async function updateProduct(product) {
+	try {
+		await axios.put(API_URL, product);
+		router.push('/');
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function deleteProduct() {
+	try {
+		await axios.delete(API_URL);
+		router.push('/');
+	} catch (error) {
+		console.error(error);
 	}
 }
 </script>
 
 <template>
-    <div class="product-detail">
-        <h2>Product Title</h2>
-        <img src="https://via.placeholder.com/150" alt="product.title" class="product-image">
-        <p>Product Descriptionp</p>
-        <p>Product Price</p>
-        <router-link to="/" class="back-button">Back</router-link>
-    </div>
+	<div class="product-detail">
+		<h2>{{ product.title }}</h2>
+		<img :src="product.image" :alt="product.title" class="product-image" />
+		<p>{{ product.description }}</p>
+		<p>Rp{{ product.price }}</p>
+		<ProductForm :product="product" @update-product="updateProduct" />
+		<router-link to="/" class="back-button">Back</router-link>
+		<button @click="deleteProduct" class="delete-button">Delete</button>
+	</div>
 </template>
 
 <style scoped>
-/* Styling untuk halaman detail produk */
 .product-detail {
 	margin-top: 20px;
 	padding: 20px;
@@ -43,23 +64,26 @@ async function fetchData(){
 	background-color: #f9f9f9;
 	text-align: center;
 }
+
 .product-image {
 	max-width: 100%;
 	height: auto;
 	margin-bottom: 10px;
 }
+
 .product-detail p {
 	margin-bottom: 5px;
 }
+
 .product-detail button {
 	margin-top: 10px;
 	padding: 10px 20px;
-	background-color: #007bff;
 	color: #fff;
 	border: none;
 	border-radius: 5px;
 	cursor: pointer;
 }
+
 .back-button {
 	display: inline-block;
 	padding: 8px 16px;
@@ -69,7 +93,22 @@ async function fetchData(){
 	border-radius: 4px;
 	transition: background-color 0.3s;
 }
+
 .back-button:hover {
 	background-color: #0056b3;
+}
+
+.delete-button {
+	display: inline-block;
+	padding: 8px 16px;
+	background-color: #ff0000;
+	color: #fff;
+	text-decoration: none;
+	border-radius: 4px;
+	transition: background-color 0.3s;
+}
+
+.delete-button:hover {
+	background-color: #b30000;
 }
 </style>
